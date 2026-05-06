f2a8f6d736e70f74919624cc4dcbeb91af6ced20
Abhijeet Kumar Shah <ajtkr200@gmail.com>
2026-05-06 13:54:54 +0530

    feat: export ALB DNS output and add Terraform state files to gitignore

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.5.0"
 
   backend "s3" {
-    bucket  = "REPLACE_WITH_YOUR_STATE_BUCKET_NAME"
+    bucket  = "realchat-prod-f5ae5800"
     key     = "terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
